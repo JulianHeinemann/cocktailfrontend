7cd626d44333fb76c8e8ab251a85eb32e50114bd
JulianHeinemann <77002383+JulianHeinemann@users.noreply.github.com>
2021-11-23 22:25:08 +0100

    Initial + Änderungen
    ...

---
+++ src/components/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">CocktailApp</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <router-link class="nav-link" to="/">HOME</router-link>
+        <router-link class="nav-link" to="/about">ABOUT</router-link>
+        <router-link class="nav-link" to="/cocktails">COCKTAILS</router-link>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Cocktails.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="card mb-3" style="max-width: 540px;">
+    <div class="row g-0">
+      <div class="col-md-4">
+        <img alt="Cocktail logo" src="../assets/Cocktail2.png">
+      </div>
+      <div class="col-md-8" style="alignment-baseline: center">
+        <div class="card-body">
+          <h5 class="card-title">Cosmopolitan</h5>
+          <p class="card-text">Einfach, Lecker und Leicht</p>
+          <button onclick="window.location.href='https://www.gutekueche.at/cosmopolitan-cocktail-rezept-17940';">Rezept</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="card mb-3" style="max-width: 540px;">
+    <div class="row g-0">
+      <div class="col-md-4">
+        <img alt="Cocktail logo" src="../assets/GinTonic.png">
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <h5 class="card-title">Gin Toinc</h5>
+          <p class="card-text">Einfach, Lecker und Leicht</p>
+          <button onclick="window.location.href='https://www.gutekueche.at/gin-tonic-cocktail-rezept-17931';">Rezept</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Cocktails'
+}
+</script>
+
+<style scoped>
+
+</style>
