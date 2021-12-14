5a78ef31a9d5856815fa44d4d179c4e80a7d7f09
TpnHTW <77002445+TpnHTW@users.noreply.github.com>
2021-12-14 23:07:26 +0100

    static json und dynamik

--- src/views/Cocktails.vue
+++ src/views/Cocktails.vue
@@ -1,37 +1,51 @@
 <template>
-  <div class="card mb-3" style="max-width: 540px;">
-    <div class="row g-0">
-      <div class="col-md-4">
-        <img alt="Cocktail logo" src="../assets/Cocktail2.png">
-      </div>
-      <div class="col-md-8" style="alignment-baseline: center">
-        <div class="card-body">
-          <h5 class="card-title">Cosmopolitan</h5>
-          <p class="card-text">Einfach, Lecker und Leicht</p>
-          <button onclick="window.location.href='https://www.gutekueche.at/cosmopolitan-cocktail-rezept-17940';">Rezept</button>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="card mb-3" style="max-width: 540px;">
-    <div class="row g-0">
-      <div class="col-md-4">
-        <img alt="Cocktail logo" src="../assets/GinTonic.png">
-      </div>
-      <div class="col-md-8">
-        <div class="card-body">
-          <h5 class="card-title">Gin Toinc</h5>
-          <p class="card-text">Einfach, Lecker und Leicht</p>
-          <button onclick="window.location.href='https://www.gutekueche.at/gin-tonic-cocktail-rezept-17931';">Rezept</button>
-        </div>
-      </div>
-    </div>
-  </div>
+     <div class="col" v-for="cocktail in cocktails" :key= "cocktail.id">
+       <div class="card mb-3" style="max-width: 540px;">
+         <div class="row g-0">
+           <div class="col-md-4">
+             <img :alt="cocktail.cocktailName" :src="getCocktailPic(cocktail)">
+         </div>
+         <div class="col-md-8" style="alignment-baseline: center">
+           <div class="card-body">
+             <h5 class="card-title">{{cocktail.cocktailName}}</h5>
+             <p class="card-text">{{cocktail.describtion}}</p>
+             <button onclick="window.location.href='https://www.gutekueche.at/cosmopolitan-cocktail-rezept-17940';">Rezept</button>
+           </div>
+         </div>
+       </div>
+     </div>
+     </div>
 </template>
 
 <script>
 export default {
-  name: 'Cocktails'
+  name: 'cocktails',
+  data () {
+    return {
+      cocktails: [
+        {
+          id: 1,
+          cocktailName: 'Cosmopolitan',
+          describtion: 'Einfach, Lecker, Leicht.'
+        },
+        {
+          id: 2,
+          cocktailName: 'Gin Tonic',
+          describtion: 'Einfach Lecker, Leicht.'
+        }
+      ]
+    }
+  },
+  methods: {
+    getCocktailPic (cocktail) {
+      switch (cocktail.id) {
+        case (1):
+          return require('../assets/GinTonic.png')
+        case (2):
+          return require('../assets/Cocktail2.png')
+      }
+    }
+  }
 }
 </script>
 
