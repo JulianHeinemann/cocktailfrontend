ef47f7df5736c9761d0c57379c6cfd1280e8ede3
TpnHTW <77002445+TpnHTW@users.noreply.github.com>
2021-12-15 23:49:00 +0100

    m4 ready

--- src/views/Cocktails.vue
+++ src/views/Cocktails.vue
@@ -3,12 +3,12 @@
        <div class="card mb-3" style="max-width: 540px;">
          <div class="row g-0">
            <div class="col-md-4">
-             <img :alt="cocktail.cocktailName" :src="getCocktailPic(cocktail)">
+             <img :alt="cocktail.name" :src="getCocktailPic(cocktail)">
          </div>
          <div class="col-md-8" style="alignment-baseline: center">
            <div class="card-body">
-             <h5 class="card-title">{{cocktail.cocktailName}}</h5>
-             <p class="card-text">{{cocktail.describtion}}</p>
+             <h5 class="card-title">{{cocktail.name}}</h5>
+             <p class="card-text">{{cocktail.zutaten}}</p>
              <button onclick="window.location.href='https://www.gutekueche.at/cosmopolitan-cocktail-rezept-17940';">Rezept</button>
            </div>
          </div>
@@ -22,18 +22,7 @@ export default {
   name: 'cocktails',
   data () {
     return {
-      cocktails: [
-        {
-          id: 1,
-          cocktailName: 'Cosmopolitan',
-          describtion: 'Einfach, Lecker, Leicht.'
-        },
-        {
-          id: 2,
-          cocktailName: 'Gin Tonic',
-          describtion: 'Einfach Lecker, Leicht.'
-        }
-      ]
+      cocktails: []
     }
   },
   methods: {
@@ -43,8 +32,24 @@ export default {
           return require('../assets/GinTonic.png')
         case (2):
           return require('../assets/Cocktail2.png')
+        default:
+          return require('../assets/Cocktail1.png')
       }
     }
+  },
+  mounted () {
+    const endpoint = process.env.VUE_APP_BACKEND_BASE_URL + 'api/v1/cocktails'
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    }
+
+    fetch(endpoint, requestOptions)
+      .then(response => response.json())
+      .then(result => result.forEach(cocktail => {
+        this.cocktails.push(cocktail)
+      }))
+      .catch(error => console.log('error', error))
   }
 }
 </script>
