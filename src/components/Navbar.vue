5a8e4293842849ed45c306402d6f93b62123b519
julianheinrich <julianhallo22@gmail.com>
2022-01-08 17:20:46 +0100

    Cocktail-Frontend

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,15 +1,25 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <nav class="navbar">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">CocktailApp</a>
+      <a class="navbar-brand" href="/">CocktailApp</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <router-link class="nav-link" to="/">HOME</router-link>
-        <router-link class="nav-link" to="/about">ABOUT</router-link>
-        <router-link class="nav-link" to="/cocktails">COCKTAILS</router-link>
+      <div class="nav-links" id="navbarNav">
+        <button class="navlinks" @click="$router.push('/')">HOME</button>
+        <button class="navlinks" @click="$router.push('/about')">ABOUT</button>
+        <button class="navlinks" @click="$router.push('/cocktails')">COCKTAILS</button>
+        <!--
+        <router-link class="navlinks" to="/">HOME</router-link>
+        <router-link class="navlinks" to="/about">ABOUT</router-link>
+        <router-link class="navlinks" to="/cocktails">COCKTAILS</router-link>
+        -->
       </div>
+      <!--
+      <div class="searchbar">
+        <input type="search" placeholder="Search..">
+      </div>
+      -->
     </div>
   </nav>
 </template>
@@ -21,5 +31,18 @@ export default {
 </script>
 
 <style scoped>
+
+.navbar-brand{
+  color: cadetblue;
+}
+.navlinks{
+  float: left;
+  margin: 5px;
+  margin-right: 100px;
+  background-color: cadetblue;
+  border: none;
+  height: 30px;
+  border-radius: 10px;
+}
 
 </style>
